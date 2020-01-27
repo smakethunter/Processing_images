26a399c3d4ea72374c87e54f4a542dc74452ff51
smaket <wojtek.pelka@gmail.com>
2020-01-27 16:41:33 +0100

    calkiem OK

--- main.cpp
+++ main.cpp
@@ -13,21 +13,30 @@ struct
 {
     std::string img_root = "/home/smaket/CLionProjects/Processing_images/imgs/";
     std::string input_filename ="lena.bmp" ;
-    std::string output_filename = "out.bmp";
+
+    std::vector< std::string> output_filename;
 
 }OP;
+
 int main() {
 //    operation data;
 // to ma przyjmować pola struktury w nazwach
+    OP.output_filename.emplace_back("out_neg.bmp");
+    OP.output_filename.emplace_back("out_filter.bmp");
+
     Image image_array = load_bitmap(OP.img_root+OP.input_filename);
     std::map<int,std:: function<void(int, int, Image&)>> queue;
     queue[1] = neg;
     queue[2] = filtr;
+    int a=0;
     for (auto& i: queue)
     {
         transform(image_array, i.second);
+        save_bitmap(OP.img_root+OP.output_filename[a], image_array);
+        transform(image_array,i.second);
+        a++;
     }
-    save_bitmap(OP.img_root+OP.output_filename, image_array);
+
 
     return EXIT_SUCCESS;
 }
